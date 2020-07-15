05187b2ac98bb8a6f9b6c3d97c970f635380bb78
ArtistSamyak <samyak.professional@gmail.com>
2020-07-15 16:56:14 +0530

    Everything working fine.

--- Currency converter/StartingViewController.swift
+++ Currency converter/StartingViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreLocation
 
+
 class StartingViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
@@ -22,27 +23,66 @@ class StartingViewController: UIViewController, CLLocationManagerDelegate {
     
 
     @IBAction func done(_ sender: UIButton) {
+        performSegue(withIdentifier: "to_main", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ViewController
+        destinationVC.list = CurrencyList.list
+        destinationVC.locationIndex = CurrencyList.locationListNumber!
+        destinationVC.baceIndex = CurrencyList.baceListNumber
     }
     
     
     func GetRates()  {
-        let url = URL(string: "https://api.exchangeratesapi.io/latest?base=\(CurrencyList.bace)")
+        let url = URL(string: "https://api.exchangeratesapi.io/latest?base=\(CurrencyList.baceCurrency)")
         var request = URLRequest(url: url!)
+        print(url!)
         request.httpMethod = "GET"
         let session = URLSession(configuration: .default)
         let dataTask = session.dataTask(with: url!) { (data, URLResponse, Error) in
             let decoder = JSONDecoder()
             
             let decodedData = try! decoder.decode(JSONStruct.self, from: data!)
-            var i = self.CurrencyList.list
-            for i in self.CurrencyList.list{
-                if i.country == self.CurrencyList.location{
-                    i.relativeRate = Float(decodedData.rates.BGN)
-                }
-            }
+            self.CurrencyList.list[0].relativeRate = Float(decodedData.rates.USD)
+            self.CurrencyList.list[1].relativeRate = Float(decodedData.rates.JPY)
+            self.CurrencyList.list[2].relativeRate = Float(decodedData.rates.BGN)
+            self.CurrencyList.list[3].relativeRate = Float(decodedData.rates.CZK)
+            self.CurrencyList.list[4].relativeRate = Float(decodedData.rates.DKK)
+            self.CurrencyList.list[5].relativeRate = Float(decodedData.rates.GBP)
+            self.CurrencyList.list[6].relativeRate = Float(decodedData.rates.HUF)
+            self.CurrencyList.list[7].relativeRate = Float(decodedData.rates.PLN)
+            self.CurrencyList.list[8].relativeRate = Float(decodedData.rates.RON)
+            self.CurrencyList.list[9].relativeRate = Float(decodedData.rates.SEK)
+            self.CurrencyList.list[10].relativeRate = Float(decodedData.rates.CHF)
+            self.CurrencyList.list[11].relativeRate = Float(decodedData.rates.ISK)
+            self.CurrencyList.list[12].relativeRate = Float(decodedData.rates.NOK)
+            self.CurrencyList.list[13].relativeRate = Float(decodedData.rates.HRK)
+            self.CurrencyList.list[14].relativeRate = Float(decodedData.rates.RUB)
+            self.CurrencyList.list[15].relativeRate = Float(decodedData.rates.TRY)
+            self.CurrencyList.list[16].relativeRate = Float(decodedData.rates.AUD)
+            self.CurrencyList.list[17].relativeRate = Float(decodedData.rates.BRL)
+            self.CurrencyList.list[18].relativeRate = Float(decodedData.rates.CAD)
+            self.CurrencyList.list[19].relativeRate = Float(decodedData.rates.CNY)
+            self.CurrencyList.list[20].relativeRate = Float(decodedData.rates.HKD)
+            self.CurrencyList.list[21].relativeRate = Float(decodedData.rates.IDR)
+            self.CurrencyList.list[22].relativeRate = Float(decodedData.rates.ILS)
+            self.CurrencyList.list[23].relativeRate = Float(decodedData.rates.INR)
+            self.CurrencyList.list[24].relativeRate = Float(decodedData.rates.KRW)
+            self.CurrencyList.list[25].relativeRate = Float(decodedData.rates.MXN)
+            self.CurrencyList.list[26].relativeRate = Float(decodedData.rates.MYR)
+            self.CurrencyList.list[27].relativeRate = Float(decodedData.rates.NZD)
+            self.CurrencyList.list[28].relativeRate = Float(decodedData.rates.PHP)
+            self.CurrencyList.list[29].relativeRate = Float(decodedData.rates.SGD)
+            self.CurrencyList.list[30].relativeRate = Float(decodedData.rates.THB)
+            self.CurrencyList.list[31].relativeRate = Float(decodedData.rates.ZAR)
+            
         }
         dataTask.resume()
     }
+    
+    
+    
     
     
     func FetchLocation(){
@@ -70,18 +110,14 @@ class StartingViewController: UIViewController, CLLocationManagerDelegate {
         let task = session.dataTask(with: url!) { (data, urlResponce, error) in
             let decoder = JSONDecoder()
             let decodedData = try! decoder.decode(LocationData.self, from: data!)
-            self.CurrencyList.location = decodedData.sys.country
+            self.CurrencyList.locationName = decodedData.sys.country
+            self.CurrencyList.GetLocationListIndex(locationName: decodedData.sys.country)
+            print(self.CurrencyList.locationListNumber!)
             print("location updated!")
         }
         task.resume()
         
     }
-    
-    
-    
-    
-    
-    
 }
 
 extension StartingViewController: UIPickerViewDelegate, UIPickerViewDataSource{
@@ -100,7 +136,8 @@ extension StartingViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print(CurrencyList.list[row].country)
-        CurrencyList.bace = CurrencyList.list[row].currency
+        CurrencyList.baceListNumber = row
+        CurrencyList.baceCurrency = CurrencyList.list[row].currency
         GetRates()
     }
 }
--- Currency converter/ViewController.swift
+++ Currency converter/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    var list : [Currency] = []
+    var locationIndex : Int = 5
+    var baceIndex : Int = 3
+    var isReverced = false
+    @IBOutlet weak var foreign: UILabel!
+    @IBOutlet weak var foreignText: UITextField!
+    @IBOutlet weak var baceCountry: UILabel!
+    @IBOutlet weak var baseAmount: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        foreign.text = list[locationIndex].country
+        baceCountry.text = list[baceIndex].country
+        baseAmount.text = "0 \(list[baceIndex].currency)"
+        foreignText.delegate = self
     }
 
-
+    @IBAction func change(_ sender: UIButton) {
+        foreignText.endEditing(true)
+        var tempText = foreign.text
+        foreign.text = baceCountry.text
+        baceCountry.text = tempText
+        
+        foreignText.text = ""
+        baseAmount.text = ""
+        isReverced = true
+        
+        
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if foreignText.text != nil {
+            let value = Float(foreignText.text!)!
+            let rate : Float
+            foreignText.text?.append(contentsOf: " \(list[locationIndex].currency)")
+            if isReverced == true {
+                rate = list[locationIndex].relativeRate
+            }else{
+                rate = 1/list[locationIndex].relativeRate
+            }
+            print(rate)
+            let amount = Float(value * rate)
+            baseAmount.text = String(format: "%.2f", amount)
+            baseAmount.text?.append(contentsOf: " \(list[baceIndex].currency)")
+            foreignText.endEditing(true)
+        }
+        
+       
+        
+        
+        return true
+    }
 }
 
